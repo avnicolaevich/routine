80e84026b806d03a977120a6e4e20574506edf0f
avnicolaevich <avnicolaevich@gmail.com>
2017-06-26 16:27:54 +0300

    add section questions and footer bg

--- app/js/common.js
+++ app/js/common.js
@@ -1,9 +1,31 @@
 $(function() {
 
-	// Custom JS
+
 
 });
 
+$(".questions__item-link").click(function (event) {
+    var target = $(event.target)[0];
+    replacePlus(target);
+    $(event.target).next().slideToggle('slow');
+});
+
+$(".questions__item-header").click(function (event) {
+    var target = $(event.target).next()[0];
+    replacePlus(target);
+    $(event.target).next().next().slideToggle('slow');
+});
+
+function replacePlus (target) {
+    var plus = target.innerText;
+    if (plus != '-') {
+        target.innerText = '-';
+        $(target.parentElement).css('border-bottom', 'none');
+    } else {
+        target.innerText = '+';
+        $(target.parentElement).css('border-bottom', '1px solid rgb(106, 79, 184)');
+    }
+}
 
 function openLogin() {
     document.getElementById("login").style.height = "100%";
